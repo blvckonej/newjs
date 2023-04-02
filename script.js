c4aecd25dd24bc53829a8398238da3532f786834
Maxim Razin <blvckonej@gmail.com>
2023-04-03 00:23:43 +0300

    finished two homework

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?');
-const gradeFilm = prompt('На сколько оцените его?');
 
-const lastFilmOne = prompt('Один из последних просмотренных фильмов?');
-const gradeFilmOne = prompt('На сколько оцените его?');
+// for (let i = 1; i <= 2; i++) {
+//     const lastFilm = prompt('Один из последних просмотренных фильмов?');
+//     const gradeFilm = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[lastFilm] = gradeFilm;
-personalMovieDB.movies[lastFilmOne] = gradeFilmOne;
+//     if (lastFilm != null && gradeFilm != null && lastFilm != '' && gradeFilm != '' && lastFilm.length < 50) {
+//         personalMovieDB.movies[lastFilm] = gradeFilm;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
 
-console.log(personalMovieDB);
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотренно довольно мало фильмов')
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы класичесский зритель')
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('Вы киноман!')
+// } else {
+//     console.log('errorFilm')
+// }
+ 
+// console.log(personalMovieDB);
